0b5f88c244becd1002915eacc56819b21ccd15df
admin <love-nankai@163.com>
2016-08-12 15:52:16 +0800

    Chainable user of alerts.

---
+++ ChainableAlert-OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChainableAlert-OC
+//
+//  Created by admin on 16/8/12.
+//  Copyright © 2016年 Ding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChainableAlert-OC/ViewController.m
@@ -0,0 +1,47 @@
+//
+//  ViewController.m
+//  ChainableAlert-OC
+//
+//  Created by admin on 16/8/12.
+//  Copyright © 2016年 Ding. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ZRDChainableAlert.h"
+#import "UIViewController+ZRDChainableAlert.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (IBAction)showAlert:(id)sender {
+    
+    [self actionSheet:@"Title" message:@"message"]
+    .normalButton(@"normal1")
+    .handler(^{
+        NSLog(@"normal1");
+    })
+    .normalButton(@"normal2")
+    .normalButton(@"normal3")
+    .destructiveButton(@"destructive1")
+    .handler (^{
+        NSLog(@"destructive1");
+    })
+    .destructiveButton(@"destructive2")
+    .cancelButton(@"cancel")
+    .show()
+    .animated(YES)
+    .completion(nil);
+    
+}
+@end
